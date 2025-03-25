e38a318e65e51600914b0cec006a0720dfa114d5
davemes1 <daveunity@gmail.com>
2025-03-25 13:57:03 -0400

    HomePage and CardPage updated

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,8 +1,16 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const Instance = axios.create({
-  baseURL: "https://evangadi-forum-backend-i25l.onrender.com/api",
-});
+// const Instance = axios.create({
+//   baseURL: "https://evangadi-forum-backend-i25l.onrender.com/api",
+// });
 
-export default Instance;
+// export default Instance;
+
+import axios from 'axios'  
+
+const Instance = axios.create({ 
+    baseURL: 'http://localhost:5500/api',
+})
+
+export default Instance
 
